01fac6c390b2425d9874393df0c64f085e78f96c
zhouyuxiao <940793261@qq.com>
2019-03-27 14:51:03 +0800

    最新框架web

--- springboot-mybatis-dubbo-zookeeper/customer/src/main/java/com/lj/customer/controller/UserController.java
+++ springboot-mybatis-dubbo-zookeeper/customer/src/main/java/com/lj/customer/controller/UserController.java
@@ -2,6 +2,7 @@ package com.lj.customer.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.lj.common.domin.User;
+import com.lj.common.service.RedisService;
 import com.lj.common.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +17,20 @@ public class UserController {
     @Resource
     @Reference(version = "1.0.0")
     private UserService userService;
+    @Resource
+    private RedisService redisservice;
 
     @GetMapping("getUser")
     public ModelAndView user(ModelAndView mv){
         System.out.println("进来了！！！！！！！！！！！！！！！！");
         System.out.println(userService.hashCode()+"!!!!!!!!!!!!!!!!");
-        List<User> user = userService.findUser();
+       // List<User> user = userService.findUser();
+        //redisservice.set("user",user);
+        List<User> user = (List<User>) redisservice.get("user");
+        for(User user1:user){
+            System.out.println("!@#@!@!!@####"+user1.getName());
+        }
+
         mv.setViewName("listuser");
         mv.addObject("user",user);
         return mv;
